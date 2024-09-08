f0ba12d33e0ff79e3f37b36d3c3607bdd2102cf0
Ahmed Hasan <121199845+ahmedhasan11@users.noreply.github.com>
2024-09-08 15:47:03 +0300

    Add files via upload

---
+++ C# Full Course .NET6 - TutorialsEU YT/Debug/debugging/debugging/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*there is something called autos , debug-windows-autos , it automatically show us the variables which are arround the current breakpoints* ,u
+                         * should use it in the debugging mood*/
+
+/* the other thing is the watch , debug-windows-watch-watch , should use it in the debugging mood , in watch u can add items like variables ,
+             and check what is their value while you debug */
+
+/*another thing is locals, locals shows you the variables that are available in the local context(in the scope we are currently looking at)
+ * ,should use it in the debugging mood*/
+ 
+
+
+
+            var friends = new List<string> {"frank","joe","michelle", "andy","maria" , "carlos" , "angelia" };
+            var partyfriends = getpartyfriends(friends, 3);
+
+            foreach(var name in partyfriends)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public static List<string> getpartyfriends(List<string> list , int count)
+        {
+            var partyfriends = new List<string>();
+
+            var buffer = new List<string>(list);
+
+            while(partyfriends.Count< count)
+            {
+                var currentfriends = getpartyfriends(buffer);
+
+                partyfriends.Add(currentfriends);
+
+                //list.Remove(currentfriends);
+                //here we are removing from the list itself , so you are deleting the element from the memory , so if 
+                //you tried to access the list , u will find that it will have no value
+
+            //solution here is  to make a copy from the list and delete from that copy
+            buffer.Remove(currentfriends);
+            
+            }
+            return partyfriends;
+        }
+
+        public static string getpartyfriends(List<string> list)
+        {
+            string shortestname = list[0];
+            for(var i = 0; i<list.Count; i++)
+            {
+                if (list[i].Length > shortestname.Length)
+                {
+                    shortestname = list[i];
+                }
+            }
+            return shortestname;
+        }
+    }
+}
---
+++ C# Full Course .NET6 - TutorialsEU YT/Exception handling/Try-Catch,exception handling/test 1 roadmap/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*to call the writesomething method in main , the method that u are calling should be static too
+            like the main method*/
+            writesomethin();
+
+            Console.WriteLine("please enter a num");
+            string input= Console.ReadLine();
+
+
+            /*here we used the try catch because if a user entered something like "aaaaaaaaaaaaaa" so thats a char or
+            string precisely , so an error will be happen when we try to ((int.parse a string)) so if the user entered
+            a num so thats it and result will happen , if user entered anything not a num . an exception will be thrown*/
+            try
+            {
+                int userinput=int.Parse(input);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("format exception please enter the correst type next time");
+            }
+
+
+
+            /* here we made another exception for the obverflow which mean that if user entered a very big number like 11
+             11111111111111111111111111111111111 so thats so big for a string to store or to handle
+            , so an error will be happen*/
+            catch (OverflowException)
+            {
+             Console.WriteLine("enter a smaller number next time");
+            }
+
+
+
+            /* here we used another catch for if the user didnt enter a value and he just pressed enter , so an
+             exception will be thrown with argument null exception or whatever u said*/
+            catch(ArgumentNullException) 
+            {
+                Console.WriteLine("value is empty , please put a value");
+            }
+
+
+
+
+            /* here Finally keyword is used when u want the window to print something whatever and exception happened or not */
+            finally 
+            {
+                Console.WriteLine("this will be done anyways");
+            }
+
+            
+        }
+        public static void writesomethin()
+        {
+            Console.WriteLine("iam called for a method");
+        }
+        public static void writesomethingspecific(string mytext) 
+        {
+        Console.WriteLine(mytext);
+        }
+    }
+}
+
