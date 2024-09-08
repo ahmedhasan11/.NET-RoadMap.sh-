5479fe2ad2e51d857993d2f8e0caf0f16e404549
Ahmed Hasan <121199845+ahmedhasan11@users.noreply.github.com>
2024-09-08 16:17:49 +0300

    Add files via upload

---
+++ ASP.NET Complete Tutorial-Intellipaat YT/LINQ/linq with databases/linq with databases/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_with_databases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeEntities entities = new EmployeeEntities();
+            var res1 = from em in entities.Emps select em;
+            foreach(var i in res1 )
+            {
+                Console.WriteLine(i.empno);
+            }
+            
+            
+        }
+    }
+}
