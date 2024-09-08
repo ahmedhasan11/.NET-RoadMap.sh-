08a002f38991070796c94bf8a387500586ae3ab0
Ahmed Hasan <121199845+ahmedhasan11@users.noreply.github.com>
2024-09-08 16:04:44 +0300

    Add files via upload

---
+++ ASP.NET Complete Tutorial-Intellipaat YT/Method Overloading/net_tutorial2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_tutorial2
+{
+    internal class Program
+    {
+        //method overloading
+
+        public void show(int x)
+        {
+            Console.WriteLine("");
+        }
+        public void show(double x)
+        {
+            Console.WriteLine("");
+        }
+        public void show(string x)
+        {
+            Console.WriteLine("");
+        }
+
+        //method overloading 2
+        public int sum(int x )
+        {
+            Console.WriteLine("");
+            return x;
+        }
+        public int sum(int x , int y)
+        {
+            Console.WriteLine("");
+            return x & y;
+        }
+        public int sum(int x , int y , int z)
+        {
+            Console.WriteLine("");
+            return x & y & z;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            //method overloading
+            obj.show(12);
+            obj.show(12.2);
+            obj.show("aaa");
+            //method overloading 2
+            obj.sum(10);
+            obj.sum(10, 20);
+            obj.sum(10, 20, 30);
+
+
+        }
+
+
+    }
+}
---
+++ ASP.NET Complete Tutorial-Intellipaat YT/Static class,methods/net tutorial 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_tutorial_3
+{
+
+    static class test //static class , must have static methods
+    {
+        public static void greeting()
+        {
+            Console.WriteLine("");
+        }
+        public static void dodododo()
+        {
+            Console.WriteLine("");
+        }
+
+    }
+    class data
+    {
+        public void show()
+        {
+            Console.WriteLine("");
+        }
+        public static void display()  //static method , called by using class name
+        {
+            Console.WriteLine("");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            data obj = new data();
+            obj.show();
+
+            data.display(); //static method , called by using class name
+        }
+    }
+}
