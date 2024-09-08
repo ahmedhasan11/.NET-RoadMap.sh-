f2dc0236ca5000e1989a7c0bd495cb3c610bae51
Ahmed Hasan <121199845+ahmedhasan11@users.noreply.github.com>
2024-09-08 15:49:08 +0300

    Add files via upload

---
+++ C# Full Course .NET6 - TutorialsEU YT/Arrays/2d array/2d array/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2D array
+            string[,] matrix;
+
+
+
+            //3D array 
+            int[,,] d3matrix;
+
+            //two-dimensional array
+
+            int[,] array2D = new int[,]
+            {
+            {1,2,3},//row 0
+            {4,5,6},//row 1
+            {7,8,9} //row 2 
+
+            };
+            Console.WriteLine("central value is {0}", array2D[1, 1]);
+
+            string[,,] array3matrix = new string[,,]
+            {
+                {
+                    {"000","001" },//00 row          third number is the order of the number in columns u want to print or get so 0 is first , 1 second
+
+                    {"010","011" },//01 row             third number is the order of the number in columns u want to print or get so 0 is first , 1 second
+
+                    { "hi there","wassup"} //02 row            third number is the order of the in columns number u want to print or get so 0 is first , 1 second
+
+                },
+
+                {
+                    {"100","101"},//10 row            third number is the order of the number in columns u want to print or get so 0 is first , 1 second
+
+                    {"110" , "111" },//11 row          third number is the order of the number in columns u want to print or get so 0 is first , 1 second
+
+                    {"another one","last entry" }//12 row        third number is the order of the numberin columns u want to print or get so 0 is first , 1 second
+
+                }
+            };
+            string[,] array2dstring = new string[3, 2]
+            {
+                { "one", "two" },
+                { "three", "four" },
+                { "five", "six" } 
+            };
+
+            array2dstring[1, 1] = "CHICKEN";
+
+            int dimensions = array2dstring.Rank; //this will gets the rank (num of dimensions of the array (its 2d or 3d))
+
+            int[,] array2d2 = { { 1, 2 }, { 3, 4 } };
+
+
+            Console.WriteLine("central value is {0}", array2dstring[1, 1]);
+        }
+    }
+}
---
+++ C# Full Course .NET6 - TutorialsEU YT/Arrays/arraylist/arraylist/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //we have to add system.collections so we can use the arraylist keyword
+
+            //declare 1 with undefined amount of objects
+            ArrayList myarraylist=new ArrayList();
+
+            //declare 2 with defined amount of objects
+            ArrayList myarraylist2 = new ArrayList(100);
+
+            myarraylist.Add(25);
+            myarraylist.Add("hello");
+            myarraylist.Add(13.37);
+            myarraylist.Add(13);
+            myarraylist.Add(128);
+            myarraylist.Add(25.3);
+            myarraylist.Add(13);
+
+            //delete element with specific value from the arraylist
+            myarraylist.Remove(13); // if we have another value called 13 in the list , it will not be romoved , this gonna remove the first 13 in the list
+
+            //delete element at a specific position (index)
+            myarraylist.RemoveAt(0);
+
+            //how many objects inside you arraylist
+            Console.WriteLine(myarraylist.Count);
+
+            double sum = 0;
+
+            foreach(object obj in myarraylist)
+            {
+                if (obj is int) //it contains an integer , but the obj is object datatype
+                {
+                    sum += Convert.ToDouble(obj); //we made the convert.todouble because the sum is a double 
+                }
+                else if (obj is double) //it contains a double , but the obj is object datatype
+                {
+                    sum += (double)obj;
+                }
+                else if (obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
